aaccf226cada7896a55381778b814142c2e67e85
TsanWeiCheng <louischeng@feds.com.tw>
2024-02-23 14:48:47 +0800

    20240223 背景動畫完成

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,27 @@ import { logo, menu, close } from '../assets';
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20`} 
+    className={`${styles.paddingX} w-full flex items-center fixed top-0 z-20 ${scrolled ? "bg-[#000f26]" : "bg-transparent"
+    }`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/' className='flex items-center gap-2' onClick={() => {
