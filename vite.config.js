b04fddedff52bd76037f6b1c45e00bb0d9619df5
TSAN WEI <a88376219@gmail.com>
2024-04-22 14:08:41 +0800

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-ThreeJs-Portfolio/",
+  base: "/React-ThreeJs-Portfolio",
   plugins: [react()],
   build: {
     outDir: 'dist'
